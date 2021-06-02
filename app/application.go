7831803024114c27b834b56a2b6a8c9e5f0a6a1e
frech <frech87@mail.ru>
2021-06-02 16:33:22 +0400

    dao structure

--- app/application.go
+++ app/application.go
@@ -1,5 +1,12 @@
 package app
 
-func StartApplication() {
+import "github.com/gin-gonic/gin"
 
+var (
+	router = gin.Default()
+)
+
+func StartApplication() {
+	mapUrls()
+	_ = router.Run(":8080")
 }
--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/frech87/bookstore_users-api/app"
+
 func main() {
 	app.StartApplication()
 }
